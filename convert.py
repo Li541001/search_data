61e304d677e424ab19a0340d1fe5e6a648daf795
Li541001 <changtibo@gmail.com>
2023-05-25 13:01:27 +0800

    add commit

--- convert.py
+++ convert.py
@@ -6,11 +6,6 @@ import tkinter
 from tkinter import filedialog
 
 
-
-# STUDENT_NUMBER = 7      #設定學生數量 需比原學生數多一人
-EXCEL_FILE_PATH = 'database.xlsx'   #excel檔案位置
-# CHOICE_COLUMN = "BCDEFG"   #輸入判斷格位
-
 def main():
   new_object = {}
   xlsx = load_workbook(path.name)
@@ -47,25 +42,6 @@ def main():
       new_object.update({id:id_object})
   #迴圈取得成績並轉換成正確形式
 
-  #  for i in range(2,STUDENT_NUMBER):     #key為標題,value為值
-  #     value = []
-  #     title = []
-  #     id = str(xlsxs['A'+str(i)].value)
-  #     id_object={}
-  #     new_object.update({id:id_object})
-  #     id_object = new_object[id]
-      
-  #     for j in CHOICE_COLUMN: 
-        
-
-  #       title = xlsxs[str(j)+'1'].value
-  #       value = str(xlsxs[str(j)+str(i)].value)
-  #       id_object.update({title:value})
-
-
-  #     new_object.update({id:id_object})
-  # #迴圈取得成績並轉換成正確形式
-
   json_object = json.dumps(new_object)  #轉成json
   print(f'giveData: {json_object}')
 
@@ -75,6 +51,8 @@ def main():
 
   response = requests.get('https://api.jsonstorage.net/v1/json/4eed805d-59cf-4ff8-ad3b-faf98c316270/c0e5a20d-e108-45d2-9097-19ef727e7af4')
   print(f'newData: {response.text}')
+  print()
+  print('------------------------------------finish----------------------------------------')
   #再次讀取database
 
 def fileStoryLocation():
@@ -82,8 +60,7 @@ def fileStoryLocation():
     pathset = filedialog.askopenfile()
     file_path.set(pathset.name)
     path = pathset
-    
-
+    #讀取電腦檔案
 
 if __name__ == '__main__':
     top = tkinter.Tk()
@@ -98,8 +75,7 @@ if __name__ == '__main__':
     submit = tkinter.Button(top,command=main,text="確定")
     submit.grid(row = 1,column = 1)
     top.mainloop()
-
-
+    #ui介面
 
 
 '''
